3c88625022b84bd905d1032ebcba29932f42133d
Md Nadim Ahmed <ahmed.nadim59@gmail.com>
2016-12-08 17:29:17 +1100

    Initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require('express'),
+    app = express(),
+    path = require('path'),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server),
+    users = [];
+
+// Set the view engine
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.engine('jade', require('jade').__express);
+
+// Set Static path
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Index Route
+app.get('/', function (req, res) {
+    res.send('Hello World');
+});
+
+server.listen(process.env.PORT || 3000);
+console.log('Server has started....');
